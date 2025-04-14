6b8728f3a26e2f618f6dac82d763e3f1eca4bb47
tsuhama <39031636+tsuhama@users.noreply.github.com>
2025-04-14 21:48:11 +0200

    feat: create backport PRs against stable version branches (#6)

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   testEnvironment: "node",
+  coverageReporters: ["json-summary", "text", "lcov"],
   testMatch: ["**/test/**/*.ts"],
   transform: {
     "^.+\.tsx?$": ["ts-jest", {}],
