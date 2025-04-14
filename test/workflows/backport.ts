4425398f9660ca98fb776579244c06a7194e1ad0
tsuhama <tsuyoshihamano94@gmail.com>
2025-04-14 21:45:45 +0200

    feat: create backport prs against stable version branches

---
+++ test/workflows/backport.ts
@@ -0,0 +1,54 @@
+import { backportFixBranch, GitControl, StableVersionMatcher } from "../../src";
+import * as maintenance from "../../src/workflows/maintenance";
+
+jest.mock("../../src/workflows/maintenance");
+
+describe("backport", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  describe("backport fix branch", () => {
+    const git: Partial<GitControl> = {
+      createPullRequest: jest.fn((title, body, source, target) =>
+        Promise.resolve({ title: title, url: "", number: 1 }),
+      ),
+      getBranch: jest.fn((name) =>
+        Promise.resolve({ name: name, sha: "", url: "" }),
+      ),
+    };
+    const matcher: Partial<StableVersionMatcher> = {};
+    const getStableVersionBranchesSpy = jest.spyOn(
+      maintenance,
+      "getStableVersionBranches",
+    );
+    test("should not create any pr, if there is no stable version branch", async () => {
+      getStableVersionBranchesSpy.mockImplementation(() => Promise.resolve([]));
+      await backportFixBranch(
+        git as GitControl,
+        matcher as StableVersionMatcher,
+        "bugfix-test",
+      );
+      expect(git.createPullRequest).not.toHaveBeenCalled();
+    });
+    test("should create pr for each stable version present", async () => {
+      getStableVersionBranchesSpy.mockImplementation(() =>
+        Promise.resolve([
+          {
+            branch: { name: "stable-1.0", sha: "", url: "" },
+            version: { major: 1, minor: 0 },
+          },
+          {
+            branch: { name: "stable-2.1", sha: "", url: "" },
+            version: { major: 2, minor: 1 },
+          },
+        ]),
+      );
+      await backportFixBranch(
+        git as GitControl,
+        matcher as StableVersionMatcher,
+        "bugfix-test",
+      );
+      expect(git.createPullRequest).toBeCalledTimes(2);
+    });
+  });
+});
