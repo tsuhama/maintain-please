1bed9e7c9b1461e5877380fc04b57a1e59d71895
tsuhama <39031636+tsuhama@users.noreply.github.com>
2025-04-15 23:32:50 +0200

    Prod dependencies cleanup (#10)
    * chore: replace release-please with octokit deps
    
    * feat!: clean up prod dependencies
    
    * chore: update coverage

--- src/core/version.ts
+++ src/core/version.ts
@@ -1,4 +1,5 @@
-import { Version } from "release-please/build/src/version.js";
+const VERSION_REGEX =
+  /(?<major>\d+)\.(?<minor>\d+)\.(?<patch>\d+)(-(?<preRelease>[^+]+))?(\+(?<build>.*))?/;
 
 export interface SemanticVersion {
   readonly patch: number;
@@ -19,10 +20,16 @@ export function toStableVersion(version: SemanticVersion): StableVersion {
 }
 
 export function parseSemanticVersion(versionValue: string): SemanticVersion {
-  const version = Version.parse(versionValue);
+  const match = versionValue.match(VERSION_REGEX);
+  if (!match?.groups) {
+    throw Error(`unable to parse version string: ${versionValue}`);
+  }
+  const major = Number(match.groups.major);
+  const minor = Number(match.groups.minor);
+  const patch = Number(match.groups.patch);
   return {
-    major: version.major,
-    minor: version.minor,
-    patch: version.patch,
+    major: major,
+    minor: minor,
+    patch: patch,
   };
 }
